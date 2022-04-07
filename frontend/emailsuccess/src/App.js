93e68460495197aff9097569b251d56582ddf47d
Aapo <aapooino@uef.fi>
2022-04-07 13:49:13 +0300

    error/success paget

---
+++ frontend/emailsuccess/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {" "}
+      Salasanan vaihto onnistui.
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="etusivun linkki"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Takaisin etusivulle
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
