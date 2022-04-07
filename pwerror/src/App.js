e359486c04b3c67a43e2df3717671e3927801d7b
Aapo <aapooino@uef.fi>
2022-04-07 13:16:52 +0300

    pw error page

---
+++ pwerror/src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Salasanan vaihtamisessa tapahtui virhe.
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="linkki etusivulle"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Takaisin etusivulle
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
